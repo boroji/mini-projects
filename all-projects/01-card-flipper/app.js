ecef79eb7bdff61214b076c994a03f3da118e21e
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-23 18:11:37 -0400

    All query selectors are working

---
+++ all-projects/01-card-flipper/app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("#generate-color");
+const hexValue = document.querySelector("#hex-value");
+const box = document.querySelector(".box");
+
+console.log(button);
+console.log(hexValue);
+console.log(box);
