1f2cfbee939808b76c2a0f8a761af439d94f480f
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-24 08:02:22 -0400

    Add change error, progress, success classes

--- all-projects/02-simple-counter/app.js
+++ all-projects/02-simple-counter/app.js
@@ -14,6 +14,25 @@ buttons.forEach(function (eachButton) {
     if (saveClasses.contains("reset-counter")) {
       countValue = 0;
     }
+    counterChangeStyles();
     updateCounter.textContent = countValue;
   });
 });
+
+function counterChangeStyles() {
+  if (countValue === 0) {
+    updateCounter.classList.add("large-progress");
+    updateCounter.classList.remove("large-error");
+    updateCounter.classList.remove("large-success");
+  }
+  if (countValue < 0) {
+    updateCounter.classList.add("large-error");
+    updateCounter.classList.remove("large-progress");
+    updateCounter.classList.remove("large-success");
+  }
+  if (countValue > 0) {
+    updateCounter.classList.add("large-success");
+    updateCounter.classList.remove("large-progress");
+    updateCounter.classList.remove("large-error");
+  }
+}
