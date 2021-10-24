b045d3b253ecbd4b0077e966722d576dc927ad61
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-23 20:37:46 -0400

    Generate random colors using hover event

--- all-projects/01-card-flipper/app.js
+++ all-projects/01-card-flipper/app.js
@@ -1,10 +1,13 @@
 const hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const button = document.querySelector("#generate-color");
+const button2 = document.querySelector("#generate-color-2");
 const hexValue = document.querySelector("#hex-value");
 const box = document.querySelector(".box");
 
 button.addEventListener("click", updateHexCode);
+button2.addEventListener("mouseover", updateHexCode);
+button2.addEventListener("mouseout", updateHexCode);
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * hexCode.length);
