80dbcb2bf66a0af28ceeac5f58d9ae1c4792334b
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-26 08:57:51 -0400

    Separate HTML buttons of remove and menu

--- all-projects/04-responsive-nav/app.js
+++ all-projects/04-responsive-nav/app.js
@@ -2,7 +2,6 @@ const button = document.querySelector(".toggle");
 const navigation = document.querySelector(".mobile-nav");
 const doubleLine = document.querySelector(".double-line");
 const tripleLine = document.querySelector(".triple-line");
-const removeLine = document.querySelector(".remove-line");
 
 button.addEventListener("click", function () {
   if (navigation.classList.contains("show-nav")) {
@@ -12,24 +11,14 @@ button.addEventListener("click", function () {
     navigation.classList.add("show-nav");
     navigation.classList.remove("hide-nav");
   }
-
-  if (tripleLine.classList.contains("slide-down")) {
-    tripleLine.classList.remove("slide-down");
-    tripleLine.classList.add("slide-up");
-  }
-  if (doubleLine.classList.contains("slide-up")) {
-    doubleLine.classList.remove("slide-down");
-    doubleLine.classList.add("slide-up");
-  }
-  console.log("click");
 });
 
 button.addEventListener("mouseover", function () {
+  doubleLine.insertBefore;
   tripleLine.classList.add("slide-down");
   tripleLine.classList.remove("slide-up");
   doubleLine.classList.add("slide-up");
   doubleLine.classList.remove("slide-down");
-  console.log("mouseover");
 });
 
 button.addEventListener("mouseout", function () {
@@ -37,5 +26,4 @@ button.addEventListener("mouseout", function () {
   doubleLine.classList.remove("slide-up");
   tripleLine.classList.add("slide-up");
   tripleLine.classList.remove("slide-down");
-  console.log("mouseout");
 });
