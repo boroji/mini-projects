a27713dcec20c5343cf61399a9567316aef7d233
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-26 11:41:44 -0400

    toggle class correct

---
+++ all-projects/05-simple-sidebar/app.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelector("section");
+const navParagraph = document.querySelectorAll("section p");
+const hambugerButton = document.querySelector(".hamburger-button");
+const removeButton = document.querySelector(".remove-button");
+
+console.log(navigation);
+console.log(navParagraph);
+console.log(hambugerButton);
+console.log(removeButton);
