9b9899b05e1d9ca5efd7fbaa9d1756c113d8e050
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-26 11:47:43 -0400

    fix the button toggle

--- all-projects/05-simple-sidebar/app.js
+++ all-projects/05-simple-sidebar/app.js
@@ -6,4 +6,8 @@ const removeButton = document.querySelector(".remove-button");
 hambugerButton.addEventListener("click", function () {
   navigation.classList.add("expand");
   navigation.classList.remove("collapse");
+  hambugerButton.classList.add("slide-left");
+  hambugerButton.classList.remove("slide-right");
+  removeButton.classList.add("slide-right");
+  removeButton.classList.remove("slide-left");
 });
