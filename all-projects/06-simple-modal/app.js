dc269244cc79b310201bb12e12a7ee3f94540fd7
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-27 07:53:28 -0400

    Modal working

--- all-projects/06-simple-modal/app.js
+++ all-projects/06-simple-modal/app.js
@@ -1,7 +1,8 @@
 const overlayColor = document.querySelector(".overlay-color");
 const removeButton = document.querySelector(".remove-button");
 const openModal = document.querySelector(".modal-open");
+const main = document.querySelector("main");
 
-console.log(overlayColor);
-console.log(removeButton);
-console.log(openModal);
+openModal.addEventListener("mousedown", function () {
+  main.classList.toggle("active");
+});
