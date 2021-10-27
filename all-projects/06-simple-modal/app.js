d9d3c238d1a0bfb577aba2bd9872c136baa4a9ae
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-27 07:55:24 -0400

    All close modal actions work

--- all-projects/06-simple-modal/app.js
+++ all-projects/06-simple-modal/app.js
@@ -3,6 +3,10 @@ const removeButton = document.querySelector(".remove-button");
 const openModal = document.querySelector(".modal-open");
 const main = document.querySelector("main");
 
-openModal.addEventListener("mousedown", function () {
+openModal.addEventListener("mousedown", closeModal);
+overlayColor.addEventListener("mousedown", closeModal);
+removeButton.addEventListener("mousedown", closeModal);
+
+function closeModal() {
   main.classList.toggle("active");
-});
+}
