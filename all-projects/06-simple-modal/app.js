b29525473cab78cf7b4ae3d6d6bab9782fb4f2f5
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-27 07:51:54 -0400

    All selected

---
+++ all-projects/06-simple-modal/app.js
@@ -0,0 +1,7 @@
+const overlayColor = document.querySelector(".overlay-color");
+const removeButton = document.querySelector(".remove-button");
+const openModal = document.querySelector(".modal-open");
+
+console.log(overlayColor);
+console.log(removeButton);
+console.log(openModal);
