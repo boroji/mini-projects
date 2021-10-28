5f66f02e578380eec29d7db274e5bf7552509546
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-28 07:53:57 -0400

    add transition to css and javascript

--- all-projects/07-simple-accordion/app.js
+++ all-projects/07-simple-accordion/app.js
@@ -2,12 +2,17 @@ const buttons = document.querySelectorAll(".toggle button");
 
 buttons.forEach(function (singleButton) {
   singleButton.addEventListener("click", function (event) {
-    let saveParagraph, saveSection, currentButton;
+    let saveParagraph, saveSection, currentButton, saveHeading;
 
     currentButton = event.currentTarget;
     saveParagraph = event.currentTarget.parentElement.parentElement.nextElementSibling;
     saveSection = event.currentTarget.parentElement.parentElement.parentElement;
+    saveHeading = event.currentTarget.parentElement.previousElementSibling;
 
+    saveHeading.classList.toggle("h6-english");
+    saveHeading.classList.toggle("h5-english");
+    saveSection.classList.toggle("inactive");
+    saveSection.classList.toggle("active");
     saveParagraph.classList.toggle("hidden");
     saveParagraph.classList.toggle("visible");
     currentButton.classList.toggle("rotate");
