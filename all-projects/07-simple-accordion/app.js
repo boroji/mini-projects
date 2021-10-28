0489f4085e6abf8f25ea5d3092080d123e4afbc9
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-28 07:46:09 -0400

    save section class

--- all-projects/07-simple-accordion/app.js
+++ all-projects/07-simple-accordion/app.js
@@ -2,9 +2,12 @@ const buttons = document.querySelectorAll(".toggle button");
 
 buttons.forEach(function (singleButton) {
   singleButton.addEventListener("click", function (event) {
-    let saveParagraph =
-      event.currentTarget.parentElement.parentElement.nextElementSibling;
-    let currentButton = event.currentTarget;
+    let saveParagraph, saveSection, currentButton;
+
+    currentButton = event.currentTarget;
+    saveParagraph = event.currentTarget.parentElement.parentElement.nextElementSibling;
+    saveSection = event.currentTarget.parentElement.parentElement.parentElement;
+
     saveParagraph.classList.toggle("hidden");
     saveParagraph.classList.toggle("visible");
     currentButton.classList.toggle("rotate");
