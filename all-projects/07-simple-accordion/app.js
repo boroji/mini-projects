d6a3bec7198194b33e180f38644f0154c88aafae
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-28 07:16:18 -0400

    Selection are correct

---
+++ all-projects/07-simple-accordion/app.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll(".toggle button");
+const paragraphs = document.querySelectorAll("section p:last-child");
+
+console.log(buttons);
+console.log(paragraphs);
