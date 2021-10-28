1bb155b4bae3c3f09aac3a1dc061e184eab10cc9
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-28 07:17:17 -0400

    Loop and current target works

--- all-projects/07-simple-accordion/app.js
+++ all-projects/07-simple-accordion/app.js
@@ -1,5 +1,8 @@
 const buttons = document.querySelectorAll(".toggle button");
 const paragraphs = document.querySelectorAll("section p:last-child");
 
-console.log(buttons);
-console.log(paragraphs);
+buttons.forEach(function (singleButton) {
+  singleButton.addEventListener("click", function (event) {
+    console.log(event.currentTarget);
+  });
+});
