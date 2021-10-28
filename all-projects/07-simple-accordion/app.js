86a0cb2f02dfb57338524797a370a874be57e01d
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-28 07:44:07 -0400

    add rotate class to rotate SVG

--- all-projects/07-simple-accordion/app.js
+++ all-projects/07-simple-accordion/app.js
@@ -1,11 +1,12 @@
 const buttons = document.querySelectorAll(".toggle button");
-const paragraphs = document.querySelectorAll("section p:last-child");
 
 buttons.forEach(function (singleButton) {
   singleButton.addEventListener("click", function (event) {
     let saveParagraph =
       event.currentTarget.parentElement.parentElement.nextElementSibling;
+    let currentButton = event.currentTarget;
     saveParagraph.classList.toggle("hidden");
     saveParagraph.classList.toggle("visible");
+    currentButton.classList.toggle("rotate");
   });
 });
