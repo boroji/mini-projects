e7850a793b57af43302c74d25615dbd1a7f21fed
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-29 15:51:49 -0400

    fix the index of review

--- all-projects/08-filter-review/app.js
+++ all-projects/08-filter-review/app.js
@@ -180,7 +180,7 @@ window.addEventListener("DOMContentLoaded", function () {
       <article>
         <img src="${singleReview.star}" alt="${singleReview.star}" />
         <p class="h5-english padding-top-16">${singleReview.firstName}</p>
-        <p class="caption-english">Review #: 0</p>
+        <p class="caption-english">Review #: ${reviews.indexOf(singleReview)}</p>
         <p class="body-english">${singleReview.review}</p>
       </article>
     </section>`;
