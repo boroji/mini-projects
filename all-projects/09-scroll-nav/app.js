9fb29b5dc3ec54a15fef3d720dfba3c78d3d2fec
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-30 09:42:48 -0400

    Add back to top button

--- all-projects/09-scroll-nav/app.js
+++ all-projects/09-scroll-nav/app.js
@@ -1,13 +1,24 @@
 const nav = document.querySelector("nav");
+const firstSection = document.querySelector(".container main:nth-child(1)");
+const buttonTop = document.querySelector("#button-top");
 
 window.addEventListener("scroll", function () {
   const viewportScroll = window.pageYOffset;
   const heightNav = nav.getBoundingClientRect().height;
+  const heightFirstSection = firstSection.getBoundingClientRect().height;
   if (viewportScroll > heightNav) {
     nav.classList.remove("inactive");
     nav.classList.add("shadow-light-bottom");
   } else {
     nav.classList.add("inactive");
     nav.classList.remove("shadow-light-bottom");
+  }
+
+  if (viewportScroll > heightFirstSection) {
+    buttonTop.classList.remove("hidden");
+    buttonTop.classList.add("visible");
+  } else {
+    buttonTop.classList.add("hidden");
+    buttonTop.classList.remove("visible");
   }
 });
