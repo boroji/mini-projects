3693d79c7a3a4f32a314abb7275441fcafc5fc12
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-31 06:57:58 -0400

    App selelction works

---
+++ all-projects/10-simple-tab/app.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll("button");
+const articles = document.querySelectorAll("article");
+const main = document.querySelectorAll("main");
+
+console.log(buttons);
+console.log(articles);
+console.log(main);
