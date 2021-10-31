c88857277e73f2a3ccc80d150862c8af1d292c4d
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-31 09:00:37 -0400

    Add set interval

--- all-projects/11-simple-timer/app.js
+++ all-projects/11-simple-timer/app.js
@@ -1,32 +1,38 @@
-let months, weekdays, year, month, day, hour, minute, second, date;
+function timer() {
+  let months, weekdays, year, month, day, hour, minute, second, date;
 
-months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+  months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-year = document.querySelector(".year");
-month = document.querySelector(".month");
-day = document.querySelector(".day");
-hour = document.querySelector(".hour");
-minute = document.querySelector(".minute");
-second = document.querySelector(".second");
+  year = document.querySelector(".year");
+  month = document.querySelector(".month");
+  day = document.querySelector(".day");
+  hour = document.querySelector(".hour");
+  minute = document.querySelector(".minute");
+  second = document.querySelector(".second");
 
-date = new Date();
+  date = new Date();
+  year.textContent = date.getFullYear();
+  month.textContent = months[date.getMonth()].toLocaleUpperCase();
+  day.textContent = weekdays[date.getDay()].toLocaleUpperCase();
+  hour.textContent = date.getHours();
+  minute.textContent = date.getMinutes();
+  second.textContent = date.getSeconds();
+}
 
-year.textContent = date.getFullYear();
-month.textContent = months[date.getMonth()].toLocaleUpperCase();
-day.textContent = weekdays[date.getDay()].toLocaleUpperCase();
-hour.textContent = date.getHours();
+setInterval(timer, 1000);
+timer();
