1cd74246bdafd269763fbe32a32ca628d88dc494
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-10-31 07:06:12 -0400

    Add active class to tabs on top

--- all-projects/10-simple-tab/app.js
+++ all-projects/10-simple-tab/app.js
@@ -3,5 +3,13 @@ const articles = document.querySelectorAll("article");
 const main = document.querySelector("main");
 
 main.addEventListener("click", function (event) {
-  console.log("ssssss");
+  const targetID = event.target.dataset.id;
+  if (targetID) {
+    buttons.forEach(function (btn) {
+      btn.classList.remove("large-primary");
+      btn.classList.add("large-secondary");
+      event.target.classList.add("large-primary");
+      event.target.classList.remove("large-secondary");
+    });
+  }
 });
