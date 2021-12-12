291727c712405a7de127cb42ab93779941e2f6b8
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-12-12 17:27:49 -0500

    Item added successfully

--- all-projects/13-to-do-list/app.js
+++ all-projects/13-to-do-list/app.js
@@ -40,7 +40,37 @@ function addItemLogic(event) {
   const generateUniqueID = new Date().getTime().toString();
   // Input logic
   if (inputValue !== "" && editToggle === false) {
-    console.log("Item added");
+    // Create element and add class to it
+    const newElement = document.createElement("article");
+    newElement.classList.add("list-item", "padding-top-16");
+
+    // Create attribute and attach it to the element
+    const newAttributeID = document.createAttribute("data-id");
+    newAttributeID.value = generateUniqueID;
+    newElement.setAttributeNode(newAttributeID);
+
+    // Add HTML content within it
+    newElement.innerHTML = `
+    <p class="h6-english">${inputValue}</p>
+     <div class="button-wrapper">
+       <button class="large-progress edit-item">
+         <svg width="24" height="24" viewBox="0 0 24 24">
+           <path
+             d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
+           />
+         </svg>
+       </button>
+       <button class="large-warning delete-item">
+         <svg width="24" height="24" viewBox="0 0 24 24">
+           <path
+             d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM19 4H15.5L14.5 3H9.5L8.5 4H5V6H19V4Z"
+           />
+         </svg>
+       </button>
+     </div>`;
+
+    // Attach the element to the HTML
+    listAll.append(newElement);
   } else if (inputValue !== "" && editToggle === true) {
     console.log("We are in edit more");
   } else {
