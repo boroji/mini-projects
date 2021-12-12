775935ae6ed20b0cfb94de5b498d271c135731d6
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-12-12 15:45:41 -0500

    All items are correctly selected

---
+++ all-projects/13-to-do-list/app.js
@@ -0,0 +1,19 @@
+const listItem = document.querySelector(".list-item");
+const writeItem = document.querySelector(".write-item");
+const addItem = document.querySelector(".add-item");
+const editItem = document.querySelector(".edit-item");
+const deleteItem = document.querySelector(".delete-item");
+const deleteAll = document.querySelector(".delete-all");
+const listAll = document.querySelector(".list-all");
+const alertSuccess = document.querySelector(".alert-success");
+const alertDelete = document.querySelector(".alert-delete");
+
+console.log(listItem);
+console.log(writeItem);
+console.log(addItem);
+console.log(editItem);
+console.log(deleteItem);
+console.log(deleteAll);
+console.log(listAll);
+console.log(alertDelete);
+console.log(alertSuccess);
