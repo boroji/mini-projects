25e7d124c4bd732de833b57550bceed2474d2c9f
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-12-19 08:15:13 -0500

    Object oriented project is initialized

---
+++ all-projects/14-counter-oop/app.js
@@ -0,0 +1,17 @@
+// function counterChangeStyles() {
+//   if (countValue === 0) {
+//     updateCounter.classList.add("large-progress");
+//     updateCounter.classList.remove("large-error");
+//     updateCounter.classList.remove("large-success");
+//   }
+//   if (countValue < 0) {
+//     updateCounter.classList.add("large-error");
+//     updateCounter.classList.remove("large-progress");
+//     updateCounter.classList.remove("large-success");
+//   }
+//   if (countValue > 0) {
+//     updateCounter.classList.add("large-success");
+//     updateCounter.classList.remove("large-progress");
+//     updateCounter.classList.remove("large-error");
+//   }
+// }
