07548b8b077e6aa4b688bc1a00bde0f34998ad86
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2021-12-20 17:23:10 -0500

    All the selections work

---
+++ all-projects/15-gallary-oop/app.js
@@ -0,0 +1,49 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`Please check "${selection}" selector, no such element exists`);
+}
+
+class Gallery {
+  constructor(element) {
+    // selecting elements
+    this.modal = getElement(".modal");
+    this.modalImg = getElement(".main-img");
+    this.imageName = getElement(".image-name");
+    this.modalImages = getElement(".modal-images");
+    this.closeBtn = getElement(".close-btn");
+    this.nextBtn = getElement(".next-btn");
+    this.prevBtn = getElement(".prev-btn");
+
+    console.log(element);
+    console.log(this.modal);
+    console.log(this.imageName);
+    console.log(this.modalImages);
+    console.log(this.closeBtn);
+    console.log(this.nextBtn);
+    console.log(this.prevBtn);
+  }
+  openModal() {
+    console.log("placeholder");
+  }
+  setMainImage() {
+    console.log("placeholder");
+  }
+  closeImage() {
+    console.log("placeholder");
+  }
+  nextImage() {
+    console.log("placeholder");
+  }
+  prevImage() {
+    console.log("placeholder");
+  }
+  chooseImage() {
+    console.log("placeholder");
+  }
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
