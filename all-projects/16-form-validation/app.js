bb9be6fbc1d03acc163720ebb9640913225e5e88
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-01-09 16:39:54 -0500

    add javascript module for selections

---
+++ all-projects/16-form-validation/app.js
@@ -0,0 +1,6 @@
+import selection from "./utils/selection.js";
+
+const email = selection(".email");
+const username = selection(".username");
+const password = selection(".password");
+const confirmPassword = selection(".confirm-password");
