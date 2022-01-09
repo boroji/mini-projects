b03ad3661373708a73bde58ce30fcb7661dde64a
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-01-09 16:41:29 -0500

    eventListener works

--- all-projects/16-form-validation/app.js
+++ all-projects/16-form-validation/app.js
@@ -4,3 +4,8 @@ const email = selection(".email");
 const username = selection(".username");
 const password = selection(".password");
 const confirmPassword = selection(".confirm-password");
+const form = selection(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
