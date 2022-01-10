c1290f733ff9b21df7ac4e2e36559da265e3fd59
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-01-10 12:20:54 -0500

    API call success

--- all-projects/17-random-user-API/app.js
+++ all-projects/17-random-user-API/app.js
@@ -1,1 +1,22 @@
 import selection from "./utils/selection.js";
+const URL = "https://randomuser.me/api/";
+const name = selection(".name");
+const email = selection(".email");
+const birthday = selection(".birthday");
+const address = selection(".address");
+const phone = selection(".phone");
+const password = selection(".password");
+const btn = selection(".btn");
+
+const getUser = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+};
+
+const showUser = () => {
+  getUser();
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
