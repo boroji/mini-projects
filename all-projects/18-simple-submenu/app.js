cb01fa6f327fbdbb26f6772a3c99492d4cddbe09
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-01-11 13:11:59 -0500

    sub menu designed

--- all-projects/18-simple-submenu/app.js
+++ all-projects/18-simple-submenu/app.js
@@ -2,9 +2,20 @@ import get from './utils/selection.js';
 import hover from './utils/hoverEvent.js';
 
 const nav = get('.nav');
+const submenu = get('.submenu');
 const btns = [
 	...nav.querySelectorAll('.main-menu'),
 ];
 
 hover(btns, 'mouseover', 'large-primary', 'large-secondary');
 hover(btns, 'mouseout', 'large-secondary', 'large-primary');
+
+btns.forEach((item) => {
+	item.addEventListener('mouseover', function(e) {
+		const bounding = e.currentTarget.getBoundingClientRect();
+		const center = (bounding.left + bounding.right) / 2;
+		submenu.classList.add('visible');
+		submenu.classList.remove('hidden');
+		submenu.style.left = `${center}px`;
+	});
+});
