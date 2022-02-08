a6ac935d32599ee334dcaa05d76110b274ae18f0
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-02-08 11:13:42 -0500

    selection and event binding worked

--- all-projects/19-animated-timer/app.js
+++ all-projects/19-animated-timer/app.js
@@ -1,1 +1,25 @@
 import selection from './utils/selection.js';
+
+class Timer {
+	constructor(startButton, pauseButton, inputChange) {
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+		this.inputChange = inputChange;
+
+		startButton.addEventListener('click', this.start);
+		pauseButton.addEventListener('click', this.pause);
+	}
+
+	start = () => {
+		console.log('this is starting');
+	};
+	pause = () => {
+		console.log('This is pausing');
+	};
+}
+
+const start = selection('#start');
+const end = selection('#end');
+const input = selection('#input');
+
+const res = new Timer(start, end, input);
