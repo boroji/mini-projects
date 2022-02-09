18d4e21c5add3ac9c15d204768f8eacb3d6a5cdd
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-02-08 20:53:20 -0500

    Add the event listener

--- all-projects/20-movie-seat-booking/app.js
+++ all-projects/20-movie-seat-booking/app.js
@@ -5,3 +5,12 @@ const container = select('.container');
 const seats = selectAll('.row .seat:not(.occupied)');
 const total = select('#total');
 const count = select('#count');
+const avengers = select('#avengers');
+
+const v = parseInt(avengers.value);
+
+container.addEventListener('click', e => {
+	if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+		e.target.classList.toggle('selected');
+	}
+});
